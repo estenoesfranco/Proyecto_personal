613807e419a5602c3d6a2a3a576ee5685318162a
estenoesfranco <112403813+estenoesfranco@users.noreply.github.com>
2023-09-04 15:12:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const boton = document.querySelector(".botoncito");
+
+console.log(boton);
+
+boton.addEventListener("click", function() {
+  alert("Wow, presionaste el botón. ¡Bien hecho!");
+});
+
+const boton2 = document.querySelector(".botoncito");
+
+console.log(boton);
+
+boton.addEventListener("click", function() {
+  alert("Wow, presionaste el botón. ¡Bien hecho!");
+});
