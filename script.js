f774e4271a4ddc2b6a1643d3d512c32647c73be6
estenoesfranco <112403813+estenoesfranco@users.noreply.github.com>
2023-09-12 12:30:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-const boton = document.querySelector(".botoncito");
+const express = require('express');
+const mongodb = require('mongodb');
+const port = 4444;
+const server = express();
 
-console.log(boton);
 
-boton.addEventListener("click", function() {
-  alert("Wow, presionaste el botón. ¡Bien hecho!");
-});
+server.get('/Saludo1', (req, res) => {
+     console.log("holaaaa")
+    });
+console.log('vos: hola mundo')
 
-const boton2 = document.querySelector(".botoncito");
 
-console.log(boton);
 
-boton.addEventListener("click", function() {
-  alert("Wow, presionaste el botón. ¡Bien hecho!");
-});
+server.post('/Saludo2', (req, res) => {
+    console.log("holaaaa que tal usuario?")
+   })
+console.log('mundo: hola usuariooo');
+
+
+server.get('/Saludo1', (req, res) => {
+    console.log("holaaaa")
+   });
+console.log('vos: como estas?')
+
+
+server.post('/Saludo2', (req, res) => {
+    console.log("holaaaa que tal usuario?")
+   })
+console.log('mundo: muy FRENETICO');
+
+server.listen(port, () => {
+    console.log(`El servidor está funcionando en http://localhost:${port}/`);
+  });
+
